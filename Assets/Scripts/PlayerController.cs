42d9d1606e012c998a153ab8dad333f7e1eebd61
EdwardossLu <50994768+EdwardossLu@users.noreply.github.com>
2019-12-10 22:26:40 +1100

    Added start and end game functionallity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("General")]
     [SerializeField] private float speed = 0;
+    [SerializeField] private GameManager manager = null;
 
     private Rigidbody2D rb;
     private bool isJumping = false;
+    private bool gameHasStarted = false;
 
-    void Start()
+    private void Awake() 
     {
-        rb = GetComponent<Rigidbody2D>();
+        Assert.IsNotNull(manager);
     }
 
-    void Update()
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Assert.IsNotNull(rb);
+
+        rb.isKinematic = true;
+    }
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
             isJumping = true;
@@ -23,11 +34,29 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate() 
     {
-        if (isJumping)
+        if (isJumping && gameHasStarted)
         {
             isJumping = false;
             rb.velocity = Vector2.zero;
             rb.AddForce((Vector2.up * speed), ForceMode2D.Impulse);
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("Pillar"))
+            manager.AddScore();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.CompareTag("Pillar"))
+            Debug.LogError("You have died!");
+    }
+
+    public void StartGame()
+    {
+        gameHasStarted = true;
+        rb.isKinematic = false;
     }
 }
