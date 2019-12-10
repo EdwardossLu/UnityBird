02d29c17b146230e46aff36655f1fd2f793f4c9c
EdwardossLu <50994768+EdwardossLu@users.noreply.github.com>
2019-12-10 17:42:57 +1100

    Updated the PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] private float speed = 0;
+
+    private Rigidbody2D rb;
+    private bool isJumping = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            isJumping = true;
+    }
+
+    private void FixedUpdate() 
+    {
+        if (isJumping)
+        {
+            isJumping = false;
+            rb.velocity = Vector2.zero;
+            rb.AddForce((Vector2.up * speed), ForceMode2D.Impulse);
+        }
+    }
+}
