b14d8ddfcf3db5c4d9eb57553b5f644a4013de7a
EdwardossLu <50994768+EdwardossLu@users.noreply.github.com>
2019-12-11 22:26:47 +1100

    Update all platforms to move when the game starts

--- Assets/Scripts/PillarController.cs
+++ Assets/Scripts/PillarController.cs
@@ -6,6 +6,8 @@ public class PillarController : MonoBehaviour
 {
     [Header("General")]
     [SerializeField] private float speed = 0f;
+    [SerializeField] private float startPlatformTimer = 2f;
+    [SerializeField] private GameManager manager = null;
     
     [Header("XAsis")]
     [SerializeField] private float pointToReset = -10f;
@@ -15,14 +17,41 @@ public class PillarController : MonoBehaviour
     [SerializeField] private float topHeight = 3f;
     [SerializeField] private float bottomHeight = -3f;
 
+    private bool gameStatus = false;
+
+    private void Start() 
+    {
+        StartCoroutine(CheckGameStatus(startPlatformTimer));
+        
+        float xAxis = Random.Range(bottomHeight, topHeight);
+        transform.position = new Vector2 (transform.position.x, xAxis);
+    }
+
     private void Update() 
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-        
+        if (gameStatus)
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
         if (transform.position.x <= pointToReset)
         {
             float xAxis = Random.Range(bottomHeight, topHeight);
             transform.position = new Vector2 (resetPoint, xAxis);
         }
+    }
+
+    public void GameOver()
+    {
+        gameStatus = false;
+    }
+
+    private IEnumerator CheckGameStatus(float waitTime)
+    {
+        while (!manager.GameStatus)
+        {
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        gameStatus = true;
+        yield return null;
     }
 }
